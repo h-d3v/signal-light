09f2340c2d9e28b3005cb36371e6c7b7ecb2d26b
H.Dev <hedio6695@gmail.com>
2024-11-25 23:23:17 -0500

    up

--- next.config.mjs
+++ next.config.mjs
@@ -6,9 +6,7 @@ const nextConfig = {
   output: "export",
   images: {
     unoptimized: true, // Disable image optimization for static hosting
-  },
-  basePath: "/signal-light", // Replace with your GitHub repository name
-  assetPrefix: "/signal-light", // Ensure assets are loaded correctly
+  }
 };
 
 export default nextConfig;

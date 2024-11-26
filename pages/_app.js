c55860c4fb646173f442bef6c74f593888f09918
H.Dev <hedio6695@gmail.com>
2024-11-26 00:19:55 -0500

    up

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import '../styles/global.css'; // Import global CSS
+import '../styles/global.css';
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;

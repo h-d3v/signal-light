4233825bcf58394b1b698a680ebd6fe231cf51ee
H.Dev <hedio6695@gmail.com>
2024-11-26 21:56:28 -0500

    up

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,7 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };

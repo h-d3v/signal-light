39347a07526e9dce8679798e9bc20e890e02c9e8
H.Dev <hedio6695@gmail.com>
2024-11-26 20:58:20 -0500

    up

--- next.config.mjs
+++ next.config.mjs
@@ -3,11 +3,12 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true, // Disable TypeScript type errors during build
   },
+  reactStrictMode: true,
   output: "export",
   images: {
     unoptimized: true, // Disable image optimization for static hosting
   },
-  assetPrefix: './', // Use relative paths for static resources
+  assetPrefix: '.', // Use relative paths for static resources
   basePath: ''
 };
 
